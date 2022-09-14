65417385acc4fdbf7928f0a4a0b3384cc6fce415
libra10042 <libra10042@gmail.com>
2022-09-14 21:00:21 +0900

    static block

--- ch8/staticBlock/StaticBlockCheck.java
+++ ch8/staticBlock/StaticBlockCheck.java
@@ -4,6 +4,8 @@ public class StaticBlockCheck {
     public static void main(String[] args) {
         StaticBlockCheck check = new StaticBlockCheck();
         check.makeStaticBlockObject();
+
+        check.makeStaticBlockObjectWithData();
     }
     public void makeStaticBlockObject(){
         System.out.println("Creating block1");
@@ -14,5 +16,17 @@ public class StaticBlockCheck {
         StaticBlock block2 = new StaticBlock();
         System.out.println("Created block2");
 
+    }
+
+
+    public void makeStaticBlockObjectWithData(){
+        System.out.println("----------------------------");
+        System.out.println("data=" + StaticBlock.getData());
+        StaticBlock block1 = new StaticBlock();
+        block1.data = 1;
+        System.out.println("-------------------");
+        StaticBlock block2 = new StaticBlock();
+        block2.data = 2;
+        System.out.println("data=" + StaticBlock.getData());
     }
 }
