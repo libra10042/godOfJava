260fba2f1dbf6fe3c52d581d85277c3d709075c4
libra10042 <libra10042@gmail.com>
2022-09-14 13:08:48 +0900

    static variable  vs variable

---
+++ ch8/ReferenceStaticVariable.java
@@ -0,0 +1,23 @@
+package ch8;
+
+public class ReferenceStaticVariable {
+
+    static String name;
+    // String name;
+    public ReferenceStaticVariable(){}
+    public ReferenceStaticVariable(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        ReferenceStaticVariable reference = new ReferenceStaticVariable();
+        reference.checkName();
+    }
+
+    public void checkName(){
+        ReferenceStaticVariable reference1 = new ReferenceStaticVariable("Sangmin");
+        System.out.println(reference1.name);
+        ReferenceStaticVariable reference2 = new ReferenceStaticVariable("Sungchoon");
+        System.out.println(reference1.name);
+    }
+}
