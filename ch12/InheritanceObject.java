6841c0fddb8fc7cf62e75b598f95d0a3eccff611
libra10042 <libra10042@gmail.com>
2022-09-17 23:10:05 +0900

    obejct

---
+++ ch12/InheritanceObject.java
@@ -0,0 +1,8 @@
+package ch12;
+
+public class InheritanceObject {
+    public static void main(String[] args) throws Exception{
+        InheritanceObject object = new InheritanceObject();
+        System.out.println(object.toString());
+    }
+}
---
+++ ch12/ToString.java
@@ -0,0 +1,14 @@
+package ch12;
+
+public class ToString {
+    public static void main(String[] args) {
+        ToString thisObject = new ToString();
+        thisObject.toStringMethod(thisObject);
+    }
+
+    public void toStringMethod(Object obj){
+        System.out.println(obj);
+        System.out.println(obj.toString());
+        System.out.println("plus " + obj);
+    }
+}
