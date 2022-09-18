ae4f146e3fd1083f8e2821fc9db11c256a50e6de
libra10042 <libra10042@gmail.com>
2022-09-18 17:57:20 +0900

    throwable

---
+++ ch14/throwblePrintStackTrace/ThrowableSample.java
@@ -0,0 +1,17 @@
+package ch14.throwblePrintStackTrace;
+
+public class ThrowableSample {
+    public static void main(String[] args) {
+        ThrowableSample sample = new ThrowableSample();
+        sample.throwable();
+    }
+    public void throwable(){
+        int[] intArray = new int[5];
+        try{
+//            intArray = null;
+            System.out.println(intArray[5]);
+        }catch(Throwable t){
+            System.out.println(t.getMessage());
+        }
+    }
+}
