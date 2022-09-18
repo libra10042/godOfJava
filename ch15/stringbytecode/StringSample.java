4f17ab89f0e9f1b9320ea63cd533cedfc9cce819
libra10042 <libra10042@gmail.com>
2022-09-18 19:58:58 +0900

    convert UTF-16 ( getBytes() method)

--- ch15/stringbytecode/StringSample.java
+++ ch15/stringbytecode/StringSample.java
@@ -4,6 +4,10 @@ public class StringSample {
     public static void main(String[] args) {
         StringSample sample = new StringSample();
         sample.convert();
+        System.out.println("----------------");
+        sample.convertUTF16();
+        System.out.println("----------------");
+        sample.convert2UTF16();
     }
     public void convert(){
         try{
@@ -15,6 +19,37 @@ public class StringSample {
             }
             System.out.println();
             String korea2 = new String(array1);
+            System.out.println(korea2);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void printByteArray(byte[] array){
+        for(byte data : array){
+            System.out.print(data + " ");
+        }
+        System.out.println();
+    }
+
+    public void convertUTF16(){
+        try{
+            String korean = "한글";
+            byte[] array1 = korean.getBytes("UTF-16");
+            printByteArray(array1);
+            String korean2 = new String(array1);
+            System.out.println(korean2);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void convert2UTF16(){
+        try{
+            String korean = "한글";
+            byte[] array1 = korean.getBytes("UTF-16");
+            printByteArray(array1);
+            String korea2 = new String(array1, "UTF-16");
             System.out.println(korea2);
         }catch(Exception e){
             e.printStackTrace();
