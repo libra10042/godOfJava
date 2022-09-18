c96a7ad586dc262ebb08a7ec431baecf496f02e2
libra10042 <libra10042@gmail.com>
2022-09-18 17:31:03 +0900

    try catch - add : arrayOutOfBoundTryCatch method

--- ch14/tryCatch/ExceptionSample.java
+++ ch14/tryCatch/ExceptionSample.java
@@ -1,13 +1,22 @@
 package ch14.tryCatch;
 
+import ch10.Polymorphism.Parent;
+
 public class ExceptionSample {
 
     public static void main(String[] args) {
         ExceptionSample sample = new ExceptionSample();
-        sample.arrayOutOfBound();
+//        sample.arrayOutOfBound();
+        sample.arrayOutOfBoundTryCatch();
     }
     public void arrayOutOfBound(){
         int[] intArray = new int[5];
         System.out.println(intArray[5]);
+    }
+    public void arrayOutOfBoundTryCatch(){
+        int[] intArray = new int[5];
+        try {
+            System.out.println(intArray[5]);
+        }catch(Exception e){}
     }
 }
