36c49bcc137b7dedf1cc42ec7184c9b3c934c854
libra10042 <libra10042@gmail.com>
2022-09-19 17:58:09 +0900

    test : daemon thread test code

--- ch25/thread_main_method/daemon_thread_ex/DaemonThread.java
+++ ch25/thread_main_method/daemon_thread_ex/DaemonThread.java
@@ -1,7 +1,7 @@
 package ch25.thread_main_method.daemon_thread_ex;
 
 /**
- * @see <a href="https://docs.oracle.com/en/java/">Daemon thread example</a>
+ * @see <a href="https://www.baeldung.com/java-daemon-thread">Daemon thread example</a>
  */
 public class DaemonThread {
     public static void main(String[] args) {
