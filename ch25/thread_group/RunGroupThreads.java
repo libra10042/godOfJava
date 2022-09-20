bd806cdabd28b21b659afd71e1485cd926d89015
libra10042 <libra10042@gmail.com>
2022-09-20 13:36:40 +0900

    group thread

---
+++ ch25/thread_group/RunGroupThreads.java
@@ -0,0 +1,37 @@
+package ch25.thread_group;
+
+import ch25.thread_control_method.SleepThread;
+
+public class RunGroupThreads {
+    public static void main(String[] args) {
+        RunGroupThreads sample = new RunGroupThreads();
+        sample.groupThread();
+    }
+    public void groupThread(){
+        try {
+            SleepThread sleep1 = new SleepThread(5000);
+            SleepThread sleep2 = new SleepThread(5000);
+
+            ThreadGroup group = new ThreadGroup("Group1");
+            Thread thread1 = new Thread(group, sleep1);
+            Thread thread2 = new Thread(group, sleep2);
+
+            thread1.start();
+            thread2.start();
+            Thread.sleep(1000);
+            System.out.println("Group name="  + group.getName());
+            int activeCount = group.activeCount();
+            System.out.println("Active count="+ activeCount);
+            group.list();
+
+            Thread[] tempThreadList = new Thread[activeCount];
+            int result = group.enumerate(tempThreadList);
+            System.out.println("Enumerate result="+ result);
+            for(Thread thread : tempThreadList){
+                System.out.println(thread);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
