d75bdc43655b565b3862047ee416e4be40b34842
libra10042 <libra10042@gmail.com>
2022-09-26 01:15:46 +0900

    immutable_implementations

---
+++ baeldung/thread/immutable_implementations/MessageService.java
@@ -0,0 +1,13 @@
+package baeldung.thread.immutable_implementations;
+
+public class MessageService {
+
+    private final String message;
+
+    public MessageService(String message){
+        this.message = message;
+    }
+
+    // standard getter
+
+}
