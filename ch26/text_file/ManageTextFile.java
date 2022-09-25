5b01a575ab7b0b9b42d3372f1b99371aafa7db6e
libra10042 <libra10042@gmail.com>
2022-09-25 16:08:35 +0900

    Read text File

---
+++ ch26/text_file/ManageTextFile.java
@@ -0,0 +1,47 @@
+package ch26.text_file;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ManageTextFile {
+    public static void main(String[] args) {
+        ManageTextFile manager = new ManageTextFile();
+        int numberCount = 10;
+        String fullPath = "C:/Users/Administrator/Desktop/GodOfJava/text1/text";
+        manager.writeFile(fullPath, numberCount);
+    }
+    public void writeFile(String fileName, int numberCount) {
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            fileWriter = new FileWriter(fileName);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            for (int loop = 0; loop <= numberCount; loop++) {
+                bufferedWriter.write(Integer.toString(loop));
+                bufferedWriter.newLine();
+            }
+            System.out.println();
+        }catch(IOException e){
+            e.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            if(bufferedWriter!=null){
+                try{
+                    bufferedWriter.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if(fileWriter!=null){
+                try {
+                    fileWriter.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
